14ffb0e93cdcd49a34b2df235c4018d3c618a243
AFRIN-NIBISHA <afrinnibisha65@gmail.com>
2026-02-03 21:19:26 +0530

    Enable static file serving for production

--- server/index.js
+++ server/index.js
@@ -1382,16 +1382,16 @@ app.put('/api/notifications/:id/read', async (req, res) => {
 });
 
 // --- DEPLOYMENT CONFIGURATION ---
-// const path = require('path');
+const path = require('path');
 
 // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
